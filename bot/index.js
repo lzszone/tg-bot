9e42612b31f299bebaf25d88f2e52d3b88447882
Zheng Li <lzszone@gmail.com>
2018-03-08 05:35:50 +0800

    add

--- bot/index.js
+++ bot/index.js
@@ -1,7 +1,7 @@
 const Bot = require('telegraf');
-const token = '463770035:AAE1Bf-MIRP4uMk7uYQjZa-B8N6Vhdjv5wg';
+const bot_token = '463770035:AAE1Bf-MIRP4uMk7uYQjZa-B8N6Vhdjv5wg';
 
-const bot = new Bot(token);
+const bot = new Bot(bot_token);
 
 bot.on('channel_post', ctx => {
   const {text, chat} = ctx.update.channel_post;
@@ -9,8 +9,8 @@ bot.on('channel_post', ctx => {
   const regex = /^\$(.*)/;
   if(regex.test(text)) {
     const token = text.match(regex)[1];
+    ctx.reply(`heyyy, ${username}, your token is ${token}`)
   }
-  ctx.reply(`heyyy, ${username}, your token is ${token}`)
 })
 
 module.exports = bot
