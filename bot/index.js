feb93f7eb6024d2978008c048766007f5d6ef1f2
Zheng Li <lzszone@gmail.com>
2018-03-10 08:23:58 +0800

    add

--- bot/index.js
+++ bot/index.js
@@ -11,16 +11,20 @@ bot.on('message',ctx => {
   const regex = /^\/(.*)/;
   if(regex.test(text)) {
     const token = text.match(regex)[1];
-    const user = await UserModel.findOne({token});
-    if(user) {
-      user.account = user.account + 188;
-      user.save()
-        .then(() => {
-          ctx.reply(`Hi, ${first_name} ${last_name}, your code: ${token} is activated successfully, send shared link to your friend right away to get your bonus.\n
-          你好，${first_name} ${last_name}, 你的验证码：${token} 已经成功激活，立刻发送分享链接给朋友获得空投奖励！\n
-          Your share link (你的分享链接):
-          http://${domain}?token=${token}`)
-        })
+    let user;
+    return UserModel.findOne({token})
+      .then(user => {
+        if(user) {
+          user.account = user.account + 188;
+          return user.save()
+        }
+      })
+      .then(() => {
+        ctx.reply(`Hi, ${first_name} ${last_name}, your code: ${token} is activated successfully, send shared link to your friend right away to get your bonus.\n
+        你好，${first_name} ${last_name}, 你的验证码：${token} 已经成功激活，立刻发送分享链接给朋友获得空投奖励！\n
+        Your share link (你的分享链接):
+        http://${domain}?token=${token}`)
+      })
     }
   }
 })
